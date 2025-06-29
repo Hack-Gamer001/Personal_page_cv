1abf48e101f1a3ba284c938a444e871ade389613
Hack-Gamer001 <119648000+Hack-Gamer001@users.noreply.github.com>
2025-06-29 23:11:40 +0000

    v5

--- components/SplineTarjeta1.tsx
+++ components/SplineTarjeta1.tsx
@@ -1,18 +1,23 @@
 // components/SplineTarjeta1.tsx
+'use client';
+
 import React from 'react';
 import dynamic from 'next/dynamic';
 
+// Import dinámico correcto sin rutas inválidas
 const Spline = dynamic(() => import('@splinetool/react-spline'), { ssr: false });
 
 type Props = {
   className?: string;
 };
+
 export default function SplineTarjeta1({ className = '' }: Props) {
   return (
-    <Spline
-      scene="https://prod.spline.design/SGvePxry1vkq5kYc/scene.splinecode"
-      className={className}
-    />
+    <div className={`relative ${className}`}>
+      <Spline
+        scene="https://prod.spline.design/SGvePxry1vkq5kYc/scene.splinecode"
+        className="absolute inset-0 w-full h-full"
+      />
+    </div>
   );
 }
-
